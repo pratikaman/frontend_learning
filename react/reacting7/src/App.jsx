8b80025b472e8de8daa5d9b7ffc2e2a5885898bf
pratikaman <pratikaman.atom@gmail.com>
2024-04-11 13:09:53 +0530

    Learning props in react

--- react/reacting7/src/App.jsx
+++ react/reacting7/src/App.jsx
@@ -3,17 +3,26 @@ import Card from './components/Card'
 
 function App() {
 
+
+
   const data = [
-    { userName: 'John Doe', profession: "Musician", img: "https://plus.unsplash.com/premium_photo-1664036154109-31e0624d29c7?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    { userName: 'Pratik', profession: "Teacher", img: "https://images.unsplash.com/photo-1539125530496-3ca408f9c2d9?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    { userName: 'Neha', profession: "Painter", img: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    { userName: 'Priya', profession: "Engineer", img: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    { userName: 'John Doe', profession: "Musician", img: "https://plus.unsplash.com/premium_photo-1664036154109-31e0624d29c7?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", follow: false},
+    { userName: 'Pratik', profession: "Teacher", img: "https://images.unsplash.com/photo-1539125530496-3ca408f9c2d9?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", follow: false},
+    { userName: 'Neha', profession: "Painter", img: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", follow: false},
+    { userName: 'Priya', profession: "Engineer", img: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", follow: false},
   ];
 
 
+  const [val, setVal] = useState(data);
+  const follow = (name) => setVal(
+    () => val.map((e)=> e.userName === name ? {...e, follow: !e.follow} : e)
+  );
+
+
   return (
-    <>
-    </>
+    <div className='w-screen h-fit min-h-screen p-4 bg-purple-800 flex justify-center items-center gap-4 flex-wrap '>
+    {val.map((e, index)=><Card key={index} name= {e.userName} profession={e.profession} img={e.img} func={follow} isfollow={e.follow}/>)}
+    </div>
   )
 }
 
--- react/reacting7/src/components/Card.jsx
+++ react/reacting7/src/components/Card.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Card() {
+function Card({name, profession, img, isfollow, func}) {
+
   return (
-    <div>Card</div>
+    <div className='w-52 h-fit bg-zinc-200 rounded-md overflow-hidden'>
+        <div className='w-full h-32 bg-sky-200'>
+            <img src={img} alt={name} className='w-full h-full object-cover'/>
+        </div>
+        <div className='w-full p-3'>
+            <h3 className='text-xl font-semibold'>{name}</h3>
+            <h5 className='text-sm '>{profession}</h5>
+            <button onClick={
+                () => func(name)
+            } className={`px-2 py-1 ${isfollow ? "bg-emerald-400" : "bg-red-400"} rounded-md text-sm mt-2`}>{isfollow ? "following" : "follow"}</button>
+        </div> 
+    </div>
   )
+}
+
+Card.propTypes = {
+    name: PropTypes.string.isRequired,
+    profession: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    func: PropTypes.func.isRequired,
+    isfollow: PropTypes.bool.isRequired,
 }
 
 export default Card
