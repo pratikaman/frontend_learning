1d84ccb7c2d21d5cf6edac97371b63d7ab9c83fb
pratikaman <pratikaman.atom@gmail.com>
2024-04-16 18:20:22 +0530

    Learning gsap scroll trigger

---
+++ gsap_and_locomotive/gsap/tut3/script.js
@@ -0,0 +1,39 @@
+
+gsap.from("#circle1",{
+    duration:2,
+    delay:1,
+    scale:0,
+    rotate:720,
+})
+
+gsap.from("#circle2",{
+    duration:2,
+    scale:0,
+    rotate:720,
+    // scrollTrigger: "#circle2"
+    scrollTrigger: {
+        trigger: "#circle2",
+        scroller: "body",
+        markers:true, // used for debugging
+        start: "top 50%",
+        end: "top 30%",
+        scrub: true,
+    }
+})
+
+gsap.from("#circle3",{
+    duration:2,
+    scale:0,
+    rotate:720,
+    // scrollTrigger: "#circle3"
+    scrollTrigger: {
+        trigger: "#circle3",
+        scroller: "body",
+        // markers:true, // used for debugging
+        start: "top 50%",
+    }
+})
+
+
+
+
